41e8e7df52ed025d206924f9981511ab09df1c43
Samuel Meuli <me@samuelmeuli.com>
2018-11-14 20:54:28 +0100

    Generate ES module

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import autoprefixer from 'autoprefixer';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 
+import pkg from './package.json';
+
 
 export default {
 	input: 'src/index.js',
 	output: [
 		{
-			file: 'lib/font-picker.js',
+			file: pkg.main,
 			format: 'umd',
 
 			// HACK: export multiple globals (FontPicker and FontManager) by adding them to window
@@ -15,13 +17,20 @@ export default {
 			extend: true
 		},
 		{
-			file: 'demo/lib/font-picker.js',
+			file: pkg.module,
+			format: 'esm'
+		},
+		{
+			file: `demo/${pkg.main}`,
 			format: 'umd',
-			sourcemap: true,
 
 			// HACK: export multiple globals (FontPicker and FontManager) by adding them to window
 			name: 'window',
 			extend: true
+		},
+		{
+			file: `demo/${pkg.module}`,
+			format: 'esm'
 		}
 	],
 	plugins: [
