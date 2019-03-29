1fd31312d527e3431ee576ba1af5a8e63486f6de
Samuel Meuli <me@samuelmeuli.com>
2019-03-29 12:53:38 +0100

    Fix `dist` folder structure
    Type definitions were placed in different directories than the Rollup bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,74 @@
 import autoprefixer from "autoprefixer";
+import fs from "fs";
 import postcss from "rollup-plugin-postcss";
 import typescript from "rollup-plugin-typescript2";
 
-import pkg from "./package.json";
+const EXTENSIONS = [".es", ".es.map", ".js", ".js.map"];
+const MANAGER_OUT_TEMP = "./dist/font-manager/FontManager";
+const MANAGER_OUT = "./dist/font-manager/font-manager/FontManager";
+const PICKER_OUT_TEMP = "./dist/font-picker/FontPicker";
+const PICKER_OUT = "./dist/font-picker/font-picker/FontPicker";
+const TS_CACHE_DIR = "node_modules/.cache/rollup-plugin-typescript2";
 
+/**
+ * Fix file organization in `dist` folder (by default, using Rollup together with Typescript project
+ * references results in type definitions being placed in different directories than the bundles)
+ */
+const cleanDist = (pathOutTemp, pathOut) => ({
+	writeBundle() {
+		EXTENSIONS.forEach(extension => {
+			if (fs.existsSync(`${pathOutTemp}${extension}`)) {
+				fs.renameSync(`${pathOutTemp}${extension}`, `${pathOut}${extension}`);
+			}
+		});
+	},
+});
+
+/**
+ * Copy font picker bundle into demo directory
+ */
+const copyDist = () => ({
+	writeBundle() {
+		fs.copyFileSync(`${PICKER_OUT}.js`, `demo/FontPicker.js`);
+	},
+});
+
+/**
+ * Rollup configuration
+ */
 export default [
 	{
 		input: "src/font-manager/FontManager.ts",
 		output: [
 			{
-				file: "dist/font-manager/FontManager.js",
+				file: `${MANAGER_OUT_TEMP}.js`,
 				format: "umd",
 				name: "FontManager",
 			},
 			{
-				file: "dist/font-manager/FontManager.es",
+				file: `${MANAGER_OUT_TEMP}.es`,
 				format: "esm",
 			},
 		],
 		plugins: [
 			typescript({
-				cacheRoot: "node_modules/.cache/rollup-plugin-typescript2",
+				cacheRoot: TS_CACHE_DIR,
 				tsconfig: "src/font-manager/tsconfig.json",
 			}),
+			cleanDist(MANAGER_OUT_TEMP, MANAGER_OUT),
 		],
 	},
 	{
 		input: "src/font-picker/FontPicker.ts",
 		output: [
-			// NPM build
 			{
-				file: pkg.main,
+				file: `${PICKER_OUT_TEMP}.js`,
 				format: "umd",
 				name: "FontPicker",
 			},
 			{
-				file: pkg.module,
+				file: `${PICKER_OUT_TEMP}.es`,
 				format: "esm",
-			},
-			// Demo
-			{
-				file: `demo/dist/FontPicker.js`,
-				format: "umd",
-				name: "FontPicker",
 			},
 		],
 		plugins: [
@@ -50,9 +76,11 @@ export default [
 				plugins: [autoprefixer],
 			}),
 			typescript({
-				cacheRoot: "node_modules/.cache/rollup-plugin-typescript2",
+				cacheRoot: TS_CACHE_DIR,
 				tsconfig: "src/font-picker/tsconfig.json",
 			}),
+			cleanDist(PICKER_OUT_TEMP, PICKER_OUT),
+			copyDist(),
 		],
 	},
 ];
