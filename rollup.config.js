9e20495e9698d0fea3bd20cf592d179ede2d660a
Samuel Meuli <me@samuelmeuli.com>
2019-08-23 11:31:03 +0200

    Switch back to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+
+import pkg from "./package.json";
+
+const EXTERNALS = ["@samuelmeuli/font-manager"];
+const GLOBALS = {
+	"@samuelmeuli/font-manager": "FontManager",
+};
+const INPUT_FILE = "./src/FontPicker.ts";
+const OUTPUT_NAME = "FontPicker";
+const TS_CACHE_ROOT = "./node_modules/.cache/rollup-plugin-typescript2/";
+
+export default [
+	// Browser configuration
+	{
+		input: INPUT_FILE,
+		output: [
+			// File for distribution
+			{
+				file: "./dist-browser/FontPicker.js",
+				format: "umd",
+				name: OUTPUT_NAME,
+			},
+			// File for demo
+			{
+				file: "./demo/dist/FontPicker.js",
+				format: "umd",
+				name: OUTPUT_NAME,
+			},
+		],
+		plugins: [
+			// Bundle `font-manager` into the package (to make it usable with a <script> tag)
+			resolve(),
+			typescript({
+				cacheRoot: TS_CACHE_ROOT,
+			}),
+		],
+	},
+
+	// NPM package configuration
+	{
+		input: INPUT_FILE,
+		output: [
+			// UMD
+			{
+				file: pkg.main,
+				format: "umd",
+				name: OUTPUT_NAME,
+				globals: GLOBALS,
+			},
+			// ES module
+			{
+				file: pkg.module,
+				format: "esm",
+				globals: GLOBALS,
+			},
+		],
+		external: EXTERNALS,
+		plugins: [
+			typescript({
+				cacheRoot: TS_CACHE_ROOT,
+				tsconfigOverride: {
+					compilerOptions: {
+						declaration: true,
+						declarationMap: true,
+					},
+				},
+			}),
+		],
+	},
+];
